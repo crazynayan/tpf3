9b0b4e6cb328d619e03a4e30df658ce98b125071
crazynayan <nayan@crazyideas.co.in>
2019-12-22 22:00:18 +0530

    Separate CreateTestData in its own Test Class

--- test/__init__.py
+++ test/__init__.py
@@ -7,51 +7,59 @@ from config import config
 
 class TestAPI(TestCase):
     TOKEN = str()
+    TEST_DATA = dict()
 
-    def setUp(self) -> None:
-        if not self.TOKEN:
-            self.TOKEN = self.authenticate()
-        self.test_data_dict = {'name': 'ETA5 Testing 123', 'seg_name': 'ETA5'}
-        response = self.post(f"/test_data", json=self.test_data_dict)
-        self.test_data_dict = response.json()
+    @classmethod
+    def setUpClass(cls) -> None:
+        if not cls.TOKEN:
+            cls.TOKEN = cls.authenticate()
+        cls.TEST_DATA = {'name': 'ETA5 Testing 123', 'seg_name': 'ETA5'}
+        response = cls.post(f"/test_data", json=cls.TEST_DATA)
+        cls.TEST_DATA = response.json()
 
-    def tearDown(self) -> None:
-        self.delete(f"/test_data/{self.test_data_dict['id']}")
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.delete(f"/test_data/{cls.TEST_DATA['id']}")
 
     @staticmethod
     def get_url(url: str) -> str:
         return f"{config.SERVER_URL}{url}"
 
-    def authenticate(self) -> str:
-        response: Response = post(self.get_url('/tokens'), auth=(config.USERNAME, config.PASSWORD))
+    @classmethod
+    def authenticate(cls) -> str:
+        response: Response = post(cls.get_url('/tokens'), auth=(config.USERNAME, config.PASSWORD))
         if response.status_code != 200:
             return str()
         token: dict = response.json()
         return token['token'] if 'token' in token else str()
 
-    def setup_authorization_header(self, parameters: dict) -> dict:
-        if not self.TOKEN:
-            self.TOKEN = self.authenticate()
-        parameters['headers'] = {'Authorization': f"Bearer {self.TOKEN}"}
+    @classmethod
+    def setup_authorization_header(cls, parameters: dict) -> dict:
+        if not cls.TOKEN:
+            cls.TOKEN = cls.authenticate()
+        parameters['headers'] = {'Authorization': f"Bearer {cls.TOKEN}"}
         return parameters
 
-    def get(self, url, **kwargs) -> Response:
-        kwargs = self.setup_authorization_header(kwargs)
-        response: Response = get(self.get_url(url), **kwargs)
+    @classmethod
+    def get(cls, url, **kwargs) -> Response:
+        kwargs = cls.setup_authorization_header(kwargs)
+        response: Response = get(cls.get_url(url), **kwargs)
         if response.status_code == 401:
-            self.TOKEN = str()
+            cls.TOKEN = str()
         return response
 
-    def post(self, url, **kwargs) -> Response:
-        kwargs = self.setup_authorization_header(kwargs)
-        response: Response = post(self.get_url(url), **kwargs)
+    @classmethod
+    def post(cls, url, **kwargs) -> Response:
+        kwargs = cls.setup_authorization_header(kwargs)
+        response: Response = post(cls.get_url(url), **kwargs)
         if response.status_code == 401:
-            self.TOKEN = str()
+            cls.TOKEN = str()
         return response
 
-    def delete(self, url, **kwargs) -> Response:
-        kwargs = self.setup_authorization_header(kwargs)
-        response: Response = delete(self.get_url(url), **kwargs)
+    @classmethod
+    def delete(cls, url, **kwargs) -> Response:
+        kwargs = cls.setup_authorization_header(kwargs)
+        response: Response = delete(cls.get_url(url), **kwargs)
         if response.status_code == 401:
-            self.TOKEN = str()
+            cls.TOKEN = str()
         return response
--- test/test_api.py
+++ test/test_api.py
@@ -1,23 +1,37 @@
 from test import TestAPI
 
 
-class TestData(TestAPI):
+class CreateTestData(TestAPI):
 
-    def test_create_test_data(self) -> None:
-        response = self.get(f"/test_data/{self.test_data_dict['id']}")
+    def test_default_created(self) -> None:
+        response = self.get(f"/test_data/{self.TEST_DATA['id']}")
         self.assertEqual(200, response.status_code)
-        self.assertDictEqual(self.test_data_dict, response.json())
+        self.assertDictEqual(self.TEST_DATA, response.json())
+
+    def test_empty(self) -> None:
         response = self.post(f"/test_data", json=dict())
         self.assertEqual(400, response.status_code)
+
+    def test_duplicate_name(self) -> None:
         response = self.post(f"/test_data", json={'name': 'ETA5 Testing 123', 'seg_name': 'ETA5'})
         self.assertEqual(400, response.status_code)
+
+    def test_3_items_in_body(self) -> None:
         response = self.post(f"/test_data", json={'name': 'ETA5 Testing 1234', 'seg_name': 'ETA5', 'id': 1})
         self.assertEqual(400, response.status_code)
+
+    def test_with_only_name(self) -> None:
         response = self.post(f"/test_data", json={'name': 'ETA5 Testing 1234'})
         self.assertEqual(400, response.status_code)
+
+    def test_with_invalid_seg(self) -> None:
         response = self.post(f"/test_data", json={'name': 'ETA5 Testing 1234', 'seg_name': 'no seg'})
         self.assertEqual(400, response.status_code)
+
+    def test_with_no_name(self) -> None:
         response = self.post(f"/test_data", json={'name': '', 'seg_name': 'ETA5'})
         self.assertEqual(400, response.status_code)
+
+    def test_with_no_seg_name(self) -> None:
         response = self.post(f"/test_data", json={'name': 'ETA5 Testing 1234', 'seg_name': ''})
         self.assertEqual(400, response.status_code)
