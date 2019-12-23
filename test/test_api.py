18bfdf55b15b52f359b74eacd669d38c6d8e0660
crazynayan <nayan@crazyideas.co.in>
2019-12-23 18:37:36 +0530

    DeleteTestData

--- test/test_api.py
+++ test/test_api.py
@@ -11,6 +11,8 @@ class CreateTestData(TestAPI):
     def test_empty(self) -> None:
         response = self.post(f"/test_data", json=dict())
         self.assertEqual(400, response.status_code)
+        self.assertDictEqual({'message': 'Invalid format of test data. Need unique name and seg_name only.',
+                              'error': 'Bad Request'}, response.json())
 
     def test_duplicate_name(self) -> None:
         response = self.post(f"/test_data", json={'name': 'ETA5 Testing 123', 'seg_name': 'ETA5'})
@@ -35,3 +37,22 @@ class CreateTestData(TestAPI):
     def test_with_no_seg_name(self) -> None:
         response = self.post(f"/test_data", json={'name': 'ETA5 Testing 1234', 'seg_name': ''})
         self.assertEqual(400, response.status_code)
+
+
+class DeleteTestData(TestAPI):
+
+    def test_delete(self):
+        response = self.delete(f"/test_data/{self.TEST_DATA['id']}")
+        self.assertEqual(200, response.status_code)
+        self.assertDictEqual({'test_data_id': self.TEST_DATA['id']}, response.json())
+        response = self.get(f"/test_data/{self.TEST_DATA['id']}")
+        self.assertEqual(404, response.status_code)
+        self.assertDictEqual({'error': 'Not Found', 'message': 'Test data id not found'}, response.json())
+
+    def test_invalid_id(self):
+        response = self.delete(f"/test_data/invalid_id")
+        self.assertEqual(404, response.status_code)
+
+    def test_empty_id(self):
+        response = self.delete(f"/test_data/")
+        self.assertEqual(404, response.status_code)
