782849c89c2844943f2b231e41e06c1aced365f0
crazynayan <nayan@crazyideas.co.in>
2019-12-23 19:05:22 +0530

    @authenticated_request

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,25 @@
+from functools import wraps
 from unittest import TestCase
 
 from requests import Response, post, get, delete
 
 from config import config
+
+
+def authenticated_request(func):
+    @wraps(func)
+    def request_wrapper(cls, url, **kwargs):
+        url = f"{config.SERVER_URL}{url}"
+        if 'auth' not in kwargs:
+            if not cls.TOKEN:
+                cls.TOKEN = cls.authenticate()
+            kwargs['headers'] = {'Authorization': f"Bearer {cls.TOKEN}"}
+        response = func(cls, url, **kwargs)
+        if response.status_code == 401:
+            cls.TOKEN = str()
+        return response
+
+    return request_wrapper
 
 
 class TestAPI(TestCase):
@@ -21,45 +38,25 @@ class TestAPI(TestCase):
     def tearDownClass(cls) -> None:
         cls.delete(f"/test_data/{cls.TEST_DATA['id']}")
 
-    @staticmethod
-    def get_url(url: str) -> str:
-        return f"{config.SERVER_URL}{url}"
-
     @classmethod
     def authenticate(cls) -> str:
-        response: Response = post(cls.get_url('/tokens'), auth=(config.USERNAME, config.PASSWORD))
+        response: Response = cls.post('/tokens', auth=(config.USERNAME, config.PASSWORD))
         if response.status_code != 200:
             return str()
         token: dict = response.json()
         return token['token'] if 'token' in token else str()
 
     @classmethod
-    def setup_authorization_header(cls, parameters: dict) -> dict:
-        if not cls.TOKEN:
-            cls.TOKEN = cls.authenticate()
-        parameters['headers'] = {'Authorization': f"Bearer {cls.TOKEN}"}
-        return parameters
+    @authenticated_request
+    def get(cls, url, **kwargs):
+        return get(url, **kwargs)
 
     @classmethod
-    def get(cls, url, **kwargs) -> Response:
-        kwargs = cls.setup_authorization_header(kwargs)
-        response: Response = get(cls.get_url(url), **kwargs)
-        if response.status_code == 401:
-            cls.TOKEN = str()
-        return response
+    @authenticated_request
+    def post(cls, url, **kwargs):
+        return post(url, **kwargs)
 
     @classmethod
-    def post(cls, url, **kwargs) -> Response:
-        kwargs = cls.setup_authorization_header(kwargs)
-        response: Response = post(cls.get_url(url), **kwargs)
-        if response.status_code == 401:
-            cls.TOKEN = str()
-        return response
-
-    @classmethod
-    def delete(cls, url, **kwargs) -> Response:
-        kwargs = cls.setup_authorization_header(kwargs)
-        response: Response = delete(cls.get_url(url), **kwargs)
-        if response.status_code == 401:
-            cls.TOKEN = str()
-        return response
+    @authenticated_request
+    def delete(cls, url, **kwargs):
+        return delete(url, **kwargs)
