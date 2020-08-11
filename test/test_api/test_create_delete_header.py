e18bf6cc64782353d4192aeb8bfd21c9646b8d49
crazynayan <nayan@crazyideas.co.in>
2020-08-11 15:13:38 +0530

    Get Test Data

--- test/test_api/test_create_delete_header.py
+++ test/test_api/test_create_delete_header.py
@@ -13,6 +13,7 @@ class CreateDeleteHeader(TestAPI):
             self.delete(f"/api/test_data", params={NAME: TestAPI.NAME})
 
     def test_create(self):
+        self.cleanup = True
         response = self.post("/api/test_data", json={NAME: TestAPI.NAME, SEG_NAME: TestAPI.SEG_NAME})
         self.assertEqual(200, response.status_code)
         response_body = response.json()
@@ -21,16 +22,15 @@ class CreateDeleteHeader(TestAPI):
         self.assertEqual(Types.INPUT_HEADER, response_body[TYPE])
         self.assertIn("id", response_body)
         self.assertEqual(4, len(response_body))
-        self.cleanup = True
 
     def test_duplicate(self):
+        self.cleanup = True
         self.post("/api/test_data", json={NAME: TestAPI.NAME, SEG_NAME: TestAPI.SEG_NAME})
         response = self.post("/api/test_data", json={NAME: TestAPI.NAME, SEG_NAME: TestAPI.SEG_NAME})
         self.assertEqual(400, response.status_code)
         response_body = response.json()
         self.assertEqual(ErrorMsg.UNIQUE, response_body[NAME])
         self.assertEqual(1, len(response_body))
-        self.cleanup = True
 
     def test_long_name_and_seg_not_in_library(self):
         body = {
@@ -46,6 +46,7 @@ class CreateDeleteHeader(TestAPI):
         self.assertEqual(2, len(response_body))
 
     def test_invalid_type_and_lower_case_seg_name_and_space_padding_in_name(self):
+        self.cleanup = True
         body = {NAME: f"  {TestAPI.NAME}  ", SEG_NAME: TestAPI.SEG_NAME.lower(), TYPE: Types.PNR}
         response = self.post("/api/test_data", json=body)
         self.assertEqual(200, response.status_code)
@@ -55,7 +56,6 @@ class CreateDeleteHeader(TestAPI):
         self.assertEqual(Types.INPUT_HEADER, response_body[TYPE])
         self.assertIn("id", response_body)
         self.assertEqual(4, len(response_body))
-        self.cleanup = True
 
     def test_empty_body(self):
         response = self.post("/api/test_data", json=dict())
@@ -106,4 +106,45 @@ class CreateDeleteHeader(TestAPI):
         self.assertEqual(404, response.status_code)
         response_body = response.json()
         self.assertEqual(ErrorMsg.NOT_FOUND, response_body[TEST_DATA])
+        self.assertEqual(1, len(response_body))
+
+    def test_get_all(self):
+        self.cleanup = True
+        second_name = f"{TestAPI.NAME} 2"
+        self.post("/api/test_data", json={NAME: TestAPI.NAME, SEG_NAME: TestAPI.SEG_NAME})
+        self.post("/api/test_data", json={NAME: second_name, SEG_NAME: TestAPI.SEG_NAME})
+        response = self.get(f"/api/test_data")
+        self.assertEqual(200, response.status_code)
+        response_body = response.json()
+        self.assertEqual(2, len(response_body))
+        self.assertEqual(4, len(response_body[0]))
+        self.assertIn("id", response_body[1])
+        self.assertEqual(second_name, next(test_data[NAME] for test_data in response_body
+                                           if test_data[NAME] == second_name))
+        self.delete("/api/test_data", params={NAME: second_name})
+
+    def test_get_by_name(self):
+        self.cleanup = True
+        self.post("/api/test_data", json={NAME: TestAPI.NAME, SEG_NAME: TestAPI.SEG_NAME})
+        response = self.get(f"/api/test_data", params={NAME: TestAPI.NAME})
+        self.assertEqual(200, response.status_code)
+        response_body = response.json()
+        self.assertEqual(1, len(response_body))
+        self.assertEqual(4, len(response_body[0]))
+        self.assertIn("id", response_body[0])
+        self.assertEqual(TestAPI.NAME, response_body[0][NAME])
+        # Invalid Name - Will return an empty list
+        response = self.get(f"/api/test_data", params={NAME: "invalid name"})
+        self.assertEqual(200, response.status_code)
+        response_body = response.json()
+        self.assertListEqual(list(), response_body)
+        # Empty Name - Will return an empty list
+        response = self.get(f"/api/test_data", params={NAME: ""})
+        self.assertEqual(200, response.status_code)
+        response_body = response.json()
+        self.assertListEqual(list(), response_body)
+        # Invalid Parameter - Will work as get all
+        response = self.get(f"/api/test_data", params={"invalid": "invalid"})
+        self.assertEqual(200, response.status_code)
+        response_body = response.json()
         self.assertEqual(1, len(response_body))
